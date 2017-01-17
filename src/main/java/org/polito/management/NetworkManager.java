cb2b8189f77b60126848062b2ef35984c1344c9f
francesco benforte <cicciob92@gmail.com>
2017-01-17 17:03:29 +0100

    1) The switch representing the netowrk is deployed on the 'createSubnet' command 2) code refactoring (added prefix to VNFs name)

--- src/main/java/org/polito/management/NetworkManager.java
+++ src/main/java/org/polito/management/NetworkManager.java
@@ -8,34 +8,48 @@ import org.apache.commons.net.util.SubnetUtils;
 import org.apache.commons.net.util.SubnetUtils.SubnetInfo;
 import org.openbaton.catalogue.nfvo.Network;
 import org.openbaton.catalogue.nfvo.Subnet;
+import org.openbaton.catalogue.nfvo.VimInstance;
 import org.openbaton.exceptions.VimDriverException;
 import org.polito.model.nffg.Nffg;
 import org.polito.model.nffg.Port;
 import org.polito.model.nffg.Vnf;
 import org.polito.model.nffg.AbstractEP.Type;
 import org.polito.model.yang.dhcp.DhcpYang;
+import org.polito.proxy.UniversalNodeProxy;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class NetworkManager {
-	private static final String NETWORK = "Network";
-	private static final String SUBNET = "Subnet";
-	private static final String MANAGEMENT_NETWORK = "ManagementNetwork";
-	private static final String MANAGEMENT_SUBNET = "ManagementSubnet";
+	private static final String NETWORK_PREFIX = "SW_";
+	private static final String SUBNET_PREFIX = "DHCP_";
+	private static final String NETWORK = "network";
+	private static final String SUBNET = "subnet";
+	private static final String MANAGEMENT_NETWORK = "manag_network";
+	private static final String MANAGEMENT_SUBNET = "manag_subnet";
 
 
 	public static void createNetwork(Nffg nffg, Network network) {
 		String vnfId = NffgManager.getNewId(nffg.getVnfs());
-		NffgManager.createVnf(nffg,vnfId,network.getName(),NETWORK,"switch",null);
 		network.setExtId(vnfId);
+		// the switch representing the network will be deployed on the createSubnet command
 	}
 
 	public static void createSubnet(Nffg nffg, Network network, Subnet subnet)
 	{
+		boolean netAlreadyDeployed = false;
+		for(Vnf vnf: NffgManager.getVnfsByDescription(nffg, NETWORK))
+			if(vnf.getId().equals(network.getExtId()))
+			{
+				netAlreadyDeployed=true;
+				break;
+			}
+		if(!netAlreadyDeployed)
+			NffgManager.createVnf(nffg,network.getExtId(),NETWORK_PREFIX + network.getName(),NETWORK,"switch",null);
+
 		String vnfId = NffgManager.getNewId(nffg.getVnfs());
-		NffgManager.createVnf(nffg,vnfId,subnet.getName(),SUBNET,"dhcp",null);
+		NffgManager.createVnf(nffg,vnfId,SUBNET_PREFIX + subnet.getName(),SUBNET,null,"configurableDhcp");
 		subnet.setExtId(vnfId);
 		String managementNetId = NffgManager.getVnfsByDescription(nffg,MANAGEMENT_NETWORK).get(0).getId();
 		NffgManager.connectVnfToVnf(nffg,vnfId,managementNetId,true);
@@ -49,7 +63,7 @@ public class NetworkManager {
 		{
 			Network net = new Network();
 			net.setExtId(vnf.getId());
-			net.setName(vnf.getName());
+			net.setName(vnf.getName().replaceAll(NETWORK_PREFIX, ""));
 			networks.add(net);
 			//TODO: Set subnets
 		}
@@ -60,8 +74,8 @@ public class NetworkManager {
 		String managementSwId = NffgManager.getNewId(nffg.getVnfs());
 		String managementDhcpId = NffgManager.getNewId(nffg.getVnfs());
 		String managementHoststackId = NffgManager.getNewId(nffg.getEndpoints());
-		NffgManager.createVnf(nffg, managementSwId, MANAGEMENT_NETWORK, MANAGEMENT_NETWORK, null, "managementSwitch");
-		NffgManager.createVnf(nffg, managementDhcpId, MANAGEMENT_SUBNET, MANAGEMENT_SUBNET, null, "managementDhcp");
+		NffgManager.createVnf(nffg, managementSwId, NETWORK_PREFIX + MANAGEMENT_NETWORK, MANAGEMENT_NETWORK, null, "managementSwitch");
+		NffgManager.createVnf(nffg, managementDhcpId, SUBNET_PREFIX + MANAGEMENT_SUBNET, MANAGEMENT_SUBNET, null, "managementDhcp");
 		NffgManager.createEndpoint(nffg,managementHoststackId,"managementHoststack",Type.HOSTSTACK, "static", "192.168.4.1");
 		NffgManager.connectVnfToVnf(nffg,managementSwId,managementDhcpId,false);
 		NffgManager.connectEndpointToVnf(nffg,managementHoststackId,managementSwId);
@@ -86,9 +100,9 @@ public class NetworkManager {
 		Vnf dhcp = NffgManager.getVnfById(nffg, subnet.getExtId());
 		for(Port port: dhcp.getPorts())
 			if(port.isTrusted())
-				YangManager.addInterface(yang, port.getName(), dhcpUserPortIp, "static", "dhcp", defaultGateway);
+				YangManager.addInterface(yang, port.getName(), "", "dhcp", "config", defaultGateway);
 			else
-				YangManager.addInterface(yang, port.getName(), "", "dhcp", "config", "");
+				YangManager.addInterface(yang, port.getName(), dhcpUserPortIp, "static", "dhcp", "");
 	}
 
 	private static final String nextIpAddress(final String input) {
